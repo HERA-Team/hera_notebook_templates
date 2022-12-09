9fb51a89024c1c37ac21ab1f00484dfe8d9f04ec
Steven Murray <steven.g.murray@asu.edu>
2022-12-09 15:20:59 -0700

    use setuptools_scm

--- hera_notebook_templates/__init__.py
+++ hera_notebook_templates/__init__.py
@@ -2,8 +2,19 @@
 # Copyright 2020 the HERA Project
 # Licensed under the MIT License
 
-from . import version
 from . import utils
 from . import utils_h1c
 
-__version__ = version.version
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:  # pragma: no cover
+    try:
+        __version__ = version("hera_notebook_templates")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = "unknown"
