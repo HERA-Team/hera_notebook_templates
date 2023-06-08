78edb06a165c4950c91ed55cda7e1d21fd009e2e
Steven Murray <steven.g.murray@asu.edu>
2023-06-08 12:44:16 -0700

    fix: errors in output from cli

--- hera_notebook_templates/cli.py
+++ hera_notebook_templates/cli.py
@@ -5,6 +5,7 @@ from . import NOTEBOOKS
 import jupyter_client
 from pathlib import Path
 import subprocess as sbp
+import os
 
 k_manager = jupyter_client.kernelspec.KernelSpecManager()
 avail_kernels = k_manager.find_kernel_specs()
@@ -27,22 +28,29 @@ def avail():
 @click.option("-f", "--formats", type=str, multiple=True, default=['html'])
 @click.option("--ipynb/--no-ipynb", default=True)
 @click.option("-o", "--output", type=str, default=None)
-@click.option('--execute-args', type=str)
-@click.option('--convert-args', type=str)
-@click.option("--params", type=str, help="Extra parameters to pass to the notebook as env vars")
-def run(notebook, kernel, timeout, formats, ipynb, output, execute_args, convert_args, params):
+@click.option("--output-dir", type=click.Path(exists=True, dir_okay=True, file_okay=False), default='.')
+@click.option('--execute-args', type=str, default='')
+@click.option('--convert-args', type=str, default='')
+@click.option("--params", type=str, default='', help="Extra parameters to pass to the notebook as env vars")
+def run(notebook, kernel, timeout, formats, ipynb, output, output_dir, execute_args, convert_args, params):
     """Use nbconvert to run a notebook."""
     nbfile = NOTEBOOKS[[nb.stem for nb in NOTEBOOKS].index(notebook)]
 
     if output is None:
-        output = Path('.') / f'{notebook}.ipynb'
-
+        output = f'{notebook}.ipynb'
+    elif not output.endswith('.ipynb'):
+        output = f'{output}.ipynb'
 
     print("Executing Notebook.")
     print(f"Got notebook params: '{params}'")
     # We might have a params string like "--opt1=this --opt2 that", make this a dict...
+    print([
+        (p.split('=') if '=' in p else p.split(" ")) for p in (' '+params.strip()).split(' --')[1:]
+    ])
     params = {
-        k.replace("-", "_").upper(): v for k, v in [p.split('=').split(" ") for p in params.split('--')]
+        k.replace("-", "_").upper(): v for k, v in [
+            (p.split('=') if '=' in p else p.split(" ")) for p in (' '+params.strip()).split(' --')[1:]
+        ]
     }
     print("Setting the env variables:")
     for k, v in params.items():
@@ -52,6 +60,7 @@ def run(notebook, kernel, timeout, formats, ipynb, output, execute_args, convert
         [
             'jupyter', 'nbconvert', 
             "--output", str(output),
+            "--output-dir", output_dir, 
             "--to", "notebook",
             "--execute",
             "--ExecutePreprocessor.timeout", str(timeout),
@@ -59,7 +68,7 @@ def run(notebook, kernel, timeout, formats, ipynb, output, execute_args, convert
             execute_args,
             str(nbfile)
         ],
-        env=params
+        env={**params, **os.environ},
     )
 
     for fmt in formats:
@@ -67,7 +76,8 @@ def run(notebook, kernel, timeout, formats, ipynb, output, execute_args, convert
         sbp.run(
             [
                 'jupyter', 'nbconvert', 
-                "--output", str(output.with_suffix(f".{fmt}")),
+                "--output", output.replace(".ipynb", f".{fmt}"),
+                "--output-dir", output_dir,
                 "--to", fmt,
                 convert_args,
                 str(output)
