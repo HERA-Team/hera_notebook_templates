ad2ef0fc60ba9d4262f3f4909b91caf1c579bb69
Steven Murray <steven.g.murray@asu.edu>
2023-06-15 15:38:36 -0700

    fix: Path is not JSON-serializable

--- hera_notebook_templates/cli.py
+++ hera_notebook_templates/cli.py
@@ -100,7 +100,6 @@ def run_notebook_factory(notebook):
         'int': int,
         'float': float,
         'bool': bool,
-        'Path': Path
     }
     params = [click.option(f"--{param.replace('_', '-')}", type=tps[v['inferred_type_name']], default=v['default'], help=v['help']) for param, v in infer.items()]
 
