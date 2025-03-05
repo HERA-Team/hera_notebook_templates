f843ea141b59f3d8dda384531538668c0a9ce4de
Steven Murray <steven.g.murray@asu.edu>
2025-03-05 18:38:52 +0100

    debug printing

--- hera_notebook_templates/cli.py
+++ hera_notebook_templates/cli.py
@@ -68,6 +68,7 @@ def run_notebook_factory(notebook):
         
         output_path = Path(ctx.obj['output_dir']) / f"{basename}.ipynb"
 
+        print("These are the input kwargs: ", kwargs)
         if ctx.obj['toml'] is not None:
             cfg = toml.load(ctx.obj['toml'])
             if ctx.obj['toml_section'] is not None:
