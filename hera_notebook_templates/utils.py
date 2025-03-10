07f149bcc0a2db536e6078e6eab4f2064fb4f49a
Steven Murray <steven.g.murray@asu.edu>
2025-03-10 12:28:16 +0100

    feat: add parse_band_str function

--- hera_notebook_templates/utils.py
+++ hera_notebook_templates/utils.py
@@ -2985,3 +2985,22 @@ class Antenna:
             return sorted(mms.items(), key=lambda item: item[1])[-1][0]            
             
         return np.nan
+
+def parse_band_str(band_str: str, freqs: np.ndarray) -> tuple[float, list[float], list[float], list[slice], list[int]]:
+    """Parse a band string into a channel width and a list of slices."""
+    df = np.median(np.diff(freqs))
+    bands = [tuple([float(edge) for edge in band.split('~')]) for band in band_str.strip().split(',')]
+    min_freqs = [(np.min(freqs[freqs >= (1e6 * b[0])]) - df / 2) / 1e6 for b in bands]
+    max_freqs = [(np.max(freqs[freqs <= (1e6 * b[1])]) + df / 2) / 1e6 for b in bands]
+    min_chan = [np.where(freqs >= 1e6 * b[0])[0][0] for b in bands]
+    max_chan = [np.where(freqs <= 1e6 * b[0])[0][-1] for b in bands]
+    band_slices = [slice(minc, maxc+1) for minc, maxc in zip(min_chan, max_chan)]
+    nchans = [maxc - minc + 1 for minc, maxc in zip(min_chan, max_chan)]
+    
+    return (
+        df,
+        min_freqs, 
+        max_freqs, 
+        band_slices,
+        nchans,
+    )
